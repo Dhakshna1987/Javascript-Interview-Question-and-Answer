d624cfd581610d23644a183ea567e695085c5d7a
Dhakshna1987 <mdhakshna@gmail.com>
2025-01-24 13:09:42 +0530

    check if a string is a palindrome

---
+++ check-palindrome.js
@@ -0,0 +1,21 @@
+//Check if a string is a palindrome
+
+//madam
+//radar
+//level
+
+function isPalindrome(str) {
+    function reverseString(name){
+        let reversed = "";
+        for( let i = str.length-1; i >=0; i--){
+            reversed += str[i];
+        }
+
+        return reversed;
+    }
+    const cleanedStr = str.toLowerCase();
+    return cleanedStr === reverseString(cleanedStr);
+}
+
+console.log(isPalindrome("level"));
+console.log(isPalindrome("tree"));
